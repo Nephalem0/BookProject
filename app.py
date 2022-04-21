10d90dfc8a75c66f028a9e10590d409b647ad725
Amit Reef <73258462+demonitee@users.noreply.github.com>
2022-04-21 15:55:37 +0300

    פ

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, request, url_for, redirect
 import books
 import customers
 import loans
 
 app = Flask(__name__)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -12,7 +13,24 @@ def index():
 
 @app.route('/addbook', methods=['POST', 'GET'])
 def add():
-    return render_template('addbook.html')
+    if request.method == 'POST':
+        book_id = int(request.form['id'])
+        book_name = str(request.form['name'])
+        book_author = str(request.form['author'])
+        book_year_published = int(request.form['year_published'])
+        book_type = int(request.form['type'])
+        
+        try:
+            books.AddBook(book_id,book_name,book_author, book_year_published, book_type)
+            return redirect('/')
+
+        except:
+            allbooks = books.Books.order_by
+            return 'There was an issue adding your book'
+
+
+    else:
+        return render_template('addbook.html')
 
 
 if __name__ == "__main__":
